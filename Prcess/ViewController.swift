d35db0168cdc7f4282599dddb764b06f3ddd3099
Alexey Chekanov <chekanov@hotmail.com>
2018-03-20 01:06:47 -0400

    Misc

---
+++ Prcess/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Prcess
+//
+//  Created by Alexey Chekanov on 3/19/18.
+//  Copyright © 2018 Alexey Chekanov. All rights reserved.
+//
+
+import UIKit
+import UIElements
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var processConrol: ProcessControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        processConrol.updateLayout()
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
